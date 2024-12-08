8ae59a829f9eaa6e8092971a4a095859f50a8bf1
Carlos Delgado <cardel87@gmail.com>
2024-12-08 15:50:35 -0500

    Fish

--- main.py
+++ main.py
@@ -4,34 +4,39 @@ Script para calificar examenes
 Fecha: Junio 9 2024
 Versión: 1.5
 """
+
 from fpdf import FPDF
 from modules.informe_docente import generarInformeDocente
 from modules.informe_estudiantes import generarInformeEstudiantes
 from modules.informe_grupal import generarInformeGrupal
 from modules.procesar import procesar
 
+
 class PDF(FPDF):
     pass  # nothing happens when it is executed.
 
-num_correctas = 11.5
-fecha = '06 de junio de 2024'
-materia = "Fundamentos de lenguajes de programación"
-examen = "Examen parcial II"
-resultados_aprendizaje = ("RA2: Aplica técnicas para representar programas, análisis lexico y semántico",)
+
+num_examenes = 2
+num_correctas = 14
+fecha = "Martes, 3 de diciembre de 2024"
+materia = "Fundamentos de programación funcional y concurrente"
+examen = "Examen final"
+resultados_aprendizaje = (
+    "RA1: Desarrolla programas en estilo funcional",
+    "RA2: Desarrolla programas paralelos",
+)
 
 
 codificacion_preguntas = [
-    [19, 14, 11, 16, 8, 5, 1, 9, 4, 0, 10, 15, 12, 17, 13],
-    [10, 0, 15, 4, 9, 14, 12, 5, 11, 2, 1, 7, 19, 6, 8],
-    [3, 5, 10, 2, 12, 13, 18, 0, 4, 11, 1, 7, 14, 15, 6],
-    [18, 3, 19, 8, 2, 17, 6, 12, 13, 15, 11, 10, 9, 14, 7],
-    [12, 10, 9, 16, 8, 18, 6, 19, 3, 0, 5, 2, 11, 7, 1],
-    [19, 11, 15, 8, 6, 5, 4, 1, 3, 14, 10, 2, 7, 9, 17],
-    [10, 8, 13, 4, 16, 5, 18, 7, 3, 11, 17, 0, 19, 15, 9],
-    [11, 14, 13, 17, 12, 8, 19, 10, 5, 0, 7, 1, 15, 9, 4]
+    [12, 15, 0, 10, 9, 13, 1, 4, 16, 3, 8, 2, 5, 11, 6],
+    [6, 13, 14, 7, 4, 2, 1, 5, 10, 9, 3, 15, 16, 11, 12],
 ]
-resultados_aprendizaje_generales = [[1,2,3,4,5,6,7,9,10,11,12,13,14,15]]
+resultados_aprendizaje_generales = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16],
+    [12, 13, 14, 15],
+]
 
+# Fin de variables iniciales del programa
 preg_res_aprendizaje = []
 
 for cod in codificacion_preguntas:
@@ -40,21 +45,69 @@ for cod in codificacion_preguntas:
         res = []
         for pre in cod:
             if pre in resultados_aprendizaje_generales[i]:
-                res.append(pre) 
+                res.append(pre)
         prog_cod.append(res)
     preg_res_aprendizaje.append(prog_cod)
 
 
-num_examenes = 8
 codificacion_examenes = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
-def todo(data, respuestas, respuestas_totales, datos_examen,resultados_aprendizaje, preg_res_aprendizaje,estudiantes,estudiantes_tipo_examen): 
-    generarInformeDocente(data, estudiantes)
-    generarInformeEstudiantes(data, respuestas_totales, resultados_aprendizaje, preg_res_aprendizaje, codificacion_examenes, num_correctas, consolidado, codificacion_preguntas, PDF)
-    generarInformeGrupal(data, respuestas, respuestas_totales, datos_examen, resultados_aprendizaje, preg_res_aprendizaje,estudiantes_tipo_examen, codificacion_examenes, consolidado, codificacion_preguntas, PDF)
 
-if __name__ == '__main__':
-    data, respuestas, respuestas_totales, datos_examen, estudiantes_tipo_examen, estudiantes, consolidado = procesar(num_correctas, num_examenes, codificacion_examenes, materia, fecha, examen, codificacion_preguntas)
+def todo(
+    data,
+    respuestas,
+    respuestas_totales,
+    datos_examen,
+    resultados_aprendizaje,
+    preg_res_aprendizaje,
+    estudiantes,
+    estudiantes_tipo_examen,
+):
+    generarInformeDocente(data, estudiantes)
+    generarInformeEstudiantes(
+        data,
+        respuestas_totales,
+        resultados_aprendizaje,
+        preg_res_aprendizaje,
+        codificacion_examenes,
+        num_correctas,
+        consolidado,
+        codificacion_preguntas,
+        PDF,
+    )
+    generarInformeGrupal(
+        data,
+        respuestas,
+        respuestas_totales,
+        datos_examen,
+        resultados_aprendizaje,
+        preg_res_aprendizaje,
+        estudiantes_tipo_examen,
+        codificacion_examenes,
+        consolidado,
+        codificacion_preguntas,
+        PDF,
+    )
+
+
+if __name__ == "__main__":
+    (
+        data,
+        respuestas,
+        respuestas_totales,
+        datos_examen,
+        estudiantes_tipo_examen,
+        estudiantes,
+        consolidado,
+    ) = procesar(
+        num_correctas,
+        num_examenes,
+        codificacion_examenes,
+        materia,
+        fecha,
+        examen,
+        codificacion_preguntas,
+    )
     print("Que desea")
     print("1. Generar todo")
     print("2. Solo informe del docente")
@@ -62,20 +115,48 @@ if __name__ == '__main__':
     print("4. Solo informe grupal")
     print("5. Buscar estudiante")
     option = int(input("Ingrese la opción: "))
-    
-	
+
     if option == 1:
-        todo(data, respuestas, respuestas_totales, datos_examen,resultados_aprendizaje, preg_res_aprendizaje, estudiantes,estudiantes_tipo_examen)
+        todo(
+            data,
+            respuestas,
+            respuestas_totales,
+            datos_examen,
+            resultados_aprendizaje,
+            preg_res_aprendizaje,
+            estudiantes,
+            estudiantes_tipo_examen,
+        )
     elif option == 2:
         generarInformeDocente(data, estudiantes)
     elif option == 3:
-        generarInformeEstudiantes(data, respuestas_totales, resultados_aprendizaje, preg_res_aprendizaje, codificacion_examenes, num_correctas, consolidado, codificacion_preguntas, PDF)
+        generarInformeEstudiantes(
+            data,
+            respuestas_totales,
+            resultados_aprendizaje,
+            preg_res_aprendizaje,
+            codificacion_examenes,
+            num_correctas,
+            consolidado,
+            codificacion_preguntas,
+            PDF,
+        )
     elif option == 4:
-        generarInformeGrupal(data, respuestas, respuestas_totales, datos_examen, resultados_aprendizaje, preg_res_aprendizaje,estudiantes_tipo_examen, codificacion_examenes, consolidado, codificacion_preguntas, PDF)
+        generarInformeGrupal(
+            data,
+            respuestas,
+            respuestas_totales,
+            datos_examen,
+            resultados_aprendizaje,
+            preg_res_aprendizaje,
+            estudiantes_tipo_examen,
+            codificacion_examenes,
+            consolidado,
+            codificacion_preguntas,
+            PDF,
+        )
     elif option == 5:
         codigo_estudiante = input("Ingrese el código del estudiante: ")
-        print(data[data["codigo"]==codigo_estudiante]["Nombre de archivo"])
+        print(data[data["codigo"] == codigo_estudiante]["Nombre de archivo"])
     else:
         print("Opcion no válida")
-    
-
--- modules/informe_estudiantes.py
+++ modules/informe_estudiantes.py
@@ -1,26 +1,41 @@
 import numpy as np
 
-def generarInformeEstudiantes(data, respuestas_totales, res_aprendizaje, preg_res_aprendizaje, codificacion_examenes, num_correctas, consolidado, codificacion_preguntas, PDF):
-    #Informe por estudiante
-    for cod,tipo_examen in zip(data["codigo"].values, data["examen"].values):
+
+def generarInformeEstudiantes(
+    data,
+    respuestas_totales,
+    res_aprendizaje,
+    preg_res_aprendizaje,
+    codificacion_examenes,
+    num_correctas,
+    consolidado,
+    codificacion_preguntas,
+    PDF,
+):
+    # Informe por estudiante
+    for cod, tipo_examen in zip(data["codigo"].values, data["examen"].values):
         respuestas = respuestas_totales[int(tipo_examen)]
-        nombrearchivo = data[data["codigo"]==cod]["Nombre de archivo"].values[0]
+        nombrearchivo = data[data["codigo"] == cod]["File name"].values[0]
         nombre = data[data["codigo"] == cod]["nombre"].values[0]
         codigo = data[data["codigo"] == cod]["codigo"].values[0]
 
         nombre_examen = codificacion_examenes[int(tipo_examen)]
-        pdf = PDF(format='legal')
-        pdf.set_author('Carlos Delgado carlos.andres.delgado@correounivalle.edu.co')
+        pdf = PDF(format="legal")
+        pdf.set_author("Carlos Delgado carlos.andres.delgado@correounivalle.edu.co")
         pdf.set_title("Informe de calificaciones")
         pdf.add_page()
-        pdf.set_font("Times","B",22)
-        pdf.text(65,10,"Informe de exámen parcial")
+        pdf.set_font("Times", "B", 22)
+        pdf.text(65, 10, "Informe de exámen parcial")
         pdf.set_font("Times", "B", 18)
-        pdf.text(5,24,"Estudiante: " + data[data["codigo"] == cod]["nombre"].values[0])
-        pdf.text(5,34,"Código: " + str(data[data["codigo"] == cod]["codigo"].values[0]))
-        pdf.text(5,44,"Nota: " + str(data[data["codigo"] == cod]["nota"].values[0]))
-        pdf.text(5,54,"Examen: " + nombre_examen)
-        pdf.text(65,64,"Informe de respuestas")
+        pdf.text(
+            5, 24, "Estudiante: " + data[data["codigo"] == cod]["nombre"].values[0]
+        )
+        pdf.text(
+            5, 34, "Código: " + str(data[data["codigo"] == cod]["codigo"].values[0])
+        )
+        pdf.text(5, 44, "Nota: " + str(data[data["codigo"] == cod]["nota"].values[0]))
+        pdf.text(5, 54, "Examen: " + nombre_examen)
+        pdf.text(65, 64, "Informe de respuestas")
 
         pdf.set_font("Times", "", 16)
         pdf.line(5, 76, 5, 68)
@@ -40,90 +55,108 @@ def generarInformeEstudiantes(data, respuestas_totales, res_aprendizaje, preg_re
         estadisticas = np.zeros(20)
         respuestas_correctas = consolidado[cod][0]
         marcadas = consolidado[cod][1]
-        
+
         for correcta_examen, marcada_examen in zip(respuestas_correctas, marcadas):
 
             correcta_examen = correcta_examen.split("|")
-            if not("ANULADA" in correcta_examen):
-                pdf.line(5, 76 +pos, 5, 68 +pos)
+            if not ("ANULADA" in correcta_examen):
+                pdf.line(5, 76 + pos, 5, 68 + pos)
                 pdf.text(7 + 12, 74 + pos, str(count))
                 pdf.line(40, 76 + pos, 40, 68 + pos)
                 pdf.text(45 + 20, 74 + pos, str(marcada_examen))
                 pdf.line(100, 76 + pos, 100, 68 + pos)
-                #Agregar otros factores
+                # Agregar otros factores
                 otros = ""
-                factor = len(otros)+3 if len(otros)>0 else 0
-                pdf.text(105 + 20 - factor, 74 + pos, "|".join(correcta_examen)+otros)
+                factor = len(otros) + 3 if len(otros) > 0 else 0
+                pdf.text(105 + 20 - factor, 74 + pos, "|".join(correcta_examen) + otros)
                 pdf.line(155, 76 + pos, 155, 68 + pos)
                 numero_correctas = 0
                 total_correctas = len(correcta_examen)
 
                 for marcada in marcada_examen.split("|"):
                     if marcada in correcta_examen:
-                        numero_correctas+=1/total_correctas
+                        numero_correctas += 1 / total_correctas
                     else:
                         if marcada != "No marcada":
-                            numero_correctas-=1/(5-total_correctas)
+                            numero_correctas -= 1 / (5 - total_correctas)
 
                 if numero_correctas < 0:
                     numero_correctas = 0
-            
+
                 if numero_correctas >= 0.5:
                     pdf.set_text_color(0, 0, 255)
-                    pdf.text(160 + 15, 74 + pos, str(round(numero_correctas*100,2))+"%")
+                    pdf.text(
+                        160 + 15, 74 + pos, str(round(numero_correctas * 100, 2)) + "%"
+                    )
                 else:
                     pdf.set_text_color(255, 0, 0)
-                pdf.text(160 + 15, 74 + pos, str(round(numero_correctas*100,2))+"%")
-                num_pregunta = codificacion_preguntas[int(tipo_examen)][count-1]
+                pdf.text(
+                    160 + 15, 74 + pos, str(round(numero_correctas * 100, 2)) + "%"
+                )
+                num_pregunta = codificacion_preguntas[int(tipo_examen)][count - 1]
                 estadisticas[num_pregunta] = numero_correctas
                 pdf.set_text_color(0, 0, 0)
                 pdf.line(5, 76 + pos, 200, 76 + pos)
                 pdf.line(200, 76 + pos, 200, 68 + pos)
-                pos+=6
-            count+=1
+                pos += 6
+            count += 1
 
         estadisticas = np.array(estadisticas)
         val_res_aprendizaje = []
         for pre in preg_res_aprendizaje[int(tipo_examen)]:
             val_res_aprendizaje.append(np.average(estadisticas[pre]))
         num_correctas_marcadas = np.sum(estadisticas)
-        pdf.text(10, 80 + pos, "Número de correctas: " + str(round(num_correctas_marcadas,2)))
+        pdf.text(
+            10,
+            80 + pos,
+            "Número de correctas: " + str(round(num_correctas_marcadas, 2)),
+        )
         pdf.text(10, 90 + pos, "Preguntas correctas para 5.0: " + str(num_correctas))
         pdf.set_font("Times", "B", 22)
-        pos+=5
+        pos += 5
         pdf.text(10, 100 + pos, "Tus habilidades 0.0 a 5.0:")
         x = 0
         y = 0
-        movx = 57 #Espacio para reporte
+        movx = 57  # Espacio para reporte
         movy = 8
         pdf.set_font("Times", "", 16)
         general = codificacion_preguntas[int(tipo_examen)]
-        for label, value, preg in zip(res_aprendizaje, val_res_aprendizaje, preg_res_aprendizaje[int(tipo_examen)]):
-            pdf.text(10 + x, 110 + pos + y, label + ": " + str(round(value*5,1)) )
+        for label, value, preg in zip(
+            res_aprendizaje, val_res_aprendizaje, preg_res_aprendizaje[int(tipo_examen)]
+        ):
+            pdf.text(10 + x, 110 + pos + y, label + ": " + str(round(value * 5, 1)))
             y += movy
             index = list(map(lambda x: general.index(x), preg))
-            pdf.text(10 + x, 110 + pos + y, "Preguntas asociadas: " + str(np.array(index)+1))
+            pdf.text(
+                10 + x,
+                110 + pos + y,
+                "Preguntas asociadas: " + str(np.array(index) + 1),
+            )
             y += movy
-        pos+=y-10
+        pos += y - 10
         pdf.set_font("Times", "B", 22)
         pdf.text(10, 130 + pos, "Mapeo de preguntas:")
 
         posx = 0
-        pos+=10
+        pos += 10
         pdf.set_font("Times", "", 16)
-        pdf.line(5, 125+pos, 202, 125+pos)
-        pdf.text(10, 130+pos, "Pregunta exámen")
-        pdf.text(10, 140+pos, "Solucionario")
-        pdf.line(5, 145+pos, 202, 145+pos)
-        pdf.line(5,125+pos, 5, 145+pos)
+        pdf.line(5, 125 + pos, 202, 125 + pos)
+        pdf.text(10, 130 + pos, "Pregunta exámen")
+        pdf.text(10, 140 + pos, "Solucionario")
+        pdf.line(5, 145 + pos, 202, 145 + pos)
+        pdf.line(5, 125 + pos, 5, 145 + pos)
 
-        for i in range(0,len(codificacion_preguntas[int(tipo_examen)])):
-            pdf.line(51+posx, 125+pos, 51+posx, 145+pos)
-            pdf.text(54+posx, 130+pos, str(i+1) )
-            pdf.text(54+posx, 140+pos, str(codificacion_preguntas[int(tipo_examen)][i]+1))
+        for i in range(0, len(codificacion_preguntas[int(tipo_examen)])):
+            pdf.line(51 + posx, 125 + pos, 51 + posx, 145 + pos)
+            pdf.text(54 + posx, 130 + pos, str(i + 1))
+            pdf.text(
+                54 + posx,
+                140 + pos,
+                str(codificacion_preguntas[int(tipo_examen)][i] + 1),
+            )
             posx += 10
-        pdf.line(202,125+pos , 202 ,145+pos)
-        pos+=30
+        pdf.line(202, 125 + pos, 202, 145 + pos)
+        pos += 30
         pdf.set_font("Times", "B", 22)
         """
         pdf.text(10, 130 + pos, "Apuntes finales:")
@@ -132,6 +165,5 @@ def generarInformeEstudiantes(data, respuestas_totales, res_aprendizaje, preg_re
         pdf.text(10, 150 + pos, "Escribe un correo a: carlos.andres.delgado@correounivalle.edu.co")
         """
         pdf.add_page()
-        pdf.image("data/procesado/" + nombrearchivo+".jpg",x=10,y=2,w=200,h=280)
-        pdf.output('output/reports/' + nombre + "-" + codigo + ".pdf", 'F')
-
+        pdf.image("data/procesado/" + nombrearchivo + ".jpg", x=10, y=2, w=200, h=280)
+        pdf.output("output/reports/" + nombre + "-" + codigo + ".pdf", "F")
