53b19653ddd3b58071f3172bc60bcb7805a982d4
Carlos Delgado <cardel87@gmail.com>
2025-06-17 22:36:38 -0500

    rubrica

--- campusvirtual/rubricas.py
+++ campusvirtual/rubricas.py
@@ -10,16 +10,16 @@ import os
 # Configura tus datos
 USERNAME = os.getenv("MOODLE_USERNAME")
 PASSWORD = os.getenv("MOODLE_PASSWORD")
-TAREA_ID = "2356729"  # Cambia por el ID real de la tarea
+TAREA_ID = "2354315"  # Cambia por el ID real de la tarea
 CSV_PATH = "rubrica.csv"  # Ruta al archivo CSV
 NIVELES = [0, 5, 10, 15]  # Puntos por nivel
 DESCRIPCION = (
-    "Rubrica del tercer taller de  Programación Funcional 2025-I Univalle Sede Tulua"
+    "Rubrica del tercer taller 2 de CSP 2025-I Univalle Sede Tulua"
 )
-NOMBRE_RUBRICA = "PFC Taller 2 2025-1"  # Nombre de la rúbrica
+NOMBRE_RUBRICA = "CSP Taller 2 2025-1"  # Nombre de la rúbrica
 
 # Inicia navegador
-driver = webdriver.Firefox()  #
+driver = webdriver.Chrome()  # Asegúrate de tener el driver de Chrome instalado
 wait = WebDriverWait(driver, 10)
 
 # 1. Iniciar sesión
